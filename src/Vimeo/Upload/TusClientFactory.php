3c6d6d1f3f981d57bc334eb5f6a21d957f087889
Carlos Díez Cobo <carlos.diez.ext@rsi.cajarural.com>
2024-10-29 09:13:00 +0100

    Add proxy

--- src/Vimeo/Upload/TusClientFactory.php
+++ src/Vimeo/Upload/TusClientFactory.php
@@ -11,9 +11,19 @@ class TusClientFactory
      * @param string $url The fully qualified url of the upload, ex: https://us-files.tus.vimeo.com/files/vimeo-a1b2c3d4
      * @return Client
      */
-    public function getTusClient(string $base_uri, string $url) : Client
+    public function getTusClient(string $base_uri, string $url, $options) : Client
     {
-        $client = new TusClient($base_uri);
+        $proxy = config('mediateca.app_proxy').":".config('mediateca.app_proxy_port');
+
+        $options = [
+            'headers' => [
+                // Tus encabezados personalizados aquí
+            ],
+            'proxy' => $proxy, // Reemplaza con tu configuración de proxy
+            'verify' => false, // O true, dependiendo de si deseas verificar los certificados
+        ];
+
+        $client = new TusClient($base_uri, $options);
         $client->setUrl($url);
         return $client;
     }
